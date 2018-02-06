51ae5e7ec3fbeec80e63a270dd0a3ddfd1b98143
Filip Stanis <fstanis@google.com>
2018-02-06 17:36:15 +0000

    use "example" gateway instead of placeholder

--- app/src/main/java/com/google/android/gms/samples/wallet/Constants.java
+++ app/src/main/java/com/google/android/gms/samples/wallet/Constants.java
@@ -24,10 +24,10 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Constants {
-    // This file contains several constants you must edit before proceeding. Once you're done,
-    // remove this static block and run the sample.
-    // Before you start, please take a look at PaymentsUtil.java to see where the constants are used
-    // and to potentially remove ones not relevant to your integration.
+    // This file contains several constants you must edit before proceeding.
+    //
+    // Please take a look at PaymentsUtil.java to see where the constants are used and to
+    // potentially remove ones not relevant to your integration.
     // Required changes:
     // 1.  Update SUPPORTED_NETWORKS and SUPPORTED_METHODS if required (consult your processor if
     //     unsure).
@@ -39,12 +39,6 @@ public class Constants {
     //     provided. You don't need to update DIRECT_TOKENIZATION_PUBLIC_KEY.
     // 5.  If you're using direct integration, please consult the documentation to learn about
     //     next steps.
-    static {
-        if (true) {
-            throw new RuntimeException("[REMOVE ME] Please edit the Constants.java file per the"
-                    + " instructions inside before trying to run this sample.");
-        }
-    }
 
     // Changing this to ENVIRONMENT_PRODUCTION will make the API return real card information.
     // Please refer to the documentation to read about the required steps needed to enable
@@ -83,22 +77,17 @@ public class Constants {
 
     // The name of your payment processor / gateway. Please refer to their documentation for
     // more information.
-    public static final String GATEWAY_TOKENIZATION_NAME = "REPLACE_ME";
+    public static final String GATEWAY_TOKENIZATION_NAME = "example";
 
     // Custom parameters required by the processor / gateway.
     // In many cases, your processor / gateway will only require a gatewayMerchantId.
     // Please refer to your processor's documentation for more information. The number of parameters
     // required and their names vary depending on the processor.
     public static final List<Pair<String, String>> GATEWAY_TOKENIZATION_PARAMETERS = Arrays.asList(
-            Pair.create("gatewayMerchantId", "REPLACE_ME")
-    );
+            Pair.create("gatewayMerchantId", "exampleGatewayMerchantId")
 
-    // Example configuration when using Stripe as processor.
-    //public static final String GATEWAY_TOKENIZATION_NAME = "stripe";
-    //public static final List<Pair<String, String>> GATEWAY_TOKENIZATION_PARAMETERS = Arrays.asList(
-    //        Pair.create("stripe:version", "5.0.0"),
-    //        Pair.create("stripe:publishableKey", "pk_test_yourstripetestkey")
-    //);
+            // Your processor may require additional parameters.
+    );
 
     // Only used for DIRECT tokenization. Can be removed when using GATEWAY tokenization.
     public static final String DIRECT_TOKENIZATION_PUBLIC_KEY = "REPLACE_ME";
