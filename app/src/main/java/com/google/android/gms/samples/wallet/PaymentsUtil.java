036ec508ecbaf470e5a53a23490d8c2bed6f4688
Nalini Sewak <nsewak@nsewak-macbookpro.roam.corp.google.com>
2018-08-30 13:27:49 -0700

    Updating to APIv2

--- app/src/main/java/com/google/android/gms/samples/wallet/PaymentsUtil.java
+++ app/src/main/java/com/google/android/gms/samples/wallet/PaymentsUtil.java
@@ -17,33 +17,35 @@
 package com.google.android.gms.samples.wallet;
 
 import android.app.Activity;
-import android.util.Pair;
-
-import com.google.android.gms.tasks.Task;
-import com.google.android.gms.wallet.CardRequirements;
-import com.google.android.gms.wallet.IsReadyToPayRequest;
-import com.google.android.gms.wallet.PaymentDataRequest;
-import com.google.android.gms.wallet.PaymentMethodTokenizationParameters;
 import com.google.android.gms.wallet.PaymentsClient;
-import com.google.android.gms.wallet.ShippingAddressRequirements;
-import com.google.android.gms.wallet.TransactionInfo;
 import com.google.android.gms.wallet.Wallet;
-import com.google.android.gms.wallet.WalletConstants;
-
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Optional;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Contains helper static methods for dealing with the Payments API.
- * <p>
- * Many of the parameters used in the code are optional and are set here merely to call out their
+ *
+ * <p>Many of the parameters used in the code are optional and are set here merely to call out their
  * existence. Please consult the documentation to learn more and feel free to remove ones not
  * relevant to your implementation.
  */
 public class PaymentsUtil {
     private static final BigDecimal MICROS = new BigDecimal(1000000d);
 
-    private PaymentsUtil() {
+    private PaymentsUtil() {}
+
+    /**
+     * Create a Google Pay API base request object with properties used in all requests.
+     *
+     * @return Google Pay API base request object.
+     * @throws JSONException
+     */
+    private static JSONObject getBaseRequest() throws JSONException {
+        return new JSONObject().put("apiVersion", 2).put("apiVersionMinor", 0);
     }
 
     /**
@@ -53,129 +55,227 @@ public class PaymentsUtil {
      * @param activity is the caller's activity.
      */
     public static PaymentsClient createPaymentsClient(Activity activity) {
-        Wallet.WalletOptions walletOptions = new Wallet.WalletOptions.Builder()
-                .setEnvironment(Constants.PAYMENTS_ENVIRONMENT)
-                .build();
+        Wallet.WalletOptions walletOptions =
+                new Wallet.WalletOptions.Builder().setEnvironment(Constants.PAYMENTS_ENVIRONMENT).build();
         return Wallet.getPaymentsClient(activity, walletOptions);
     }
 
     /**
-     * Builds {@link PaymentDataRequest} to be consumed by {@link PaymentsClient#loadPaymentData}.
+     * Gateway Integration: Identify your gateway and your site's gateway merchant identifier.
      *
-     * @param transactionInfo contains the price for this transaction.
+     * <p>The Google Pay API response will return an encrypted payment method capable of being charged
+     * by a supported gateway after payer authorization.
+     *
+     * <p>TODO: Check with your gateway on the parameters to pass and modify them in Constants.java.
+     *
+     * @return Payment data tokenization for the CARD payment method.
+     * @throws JSONException
+     * @see <a href=
+     *     "https://developers.google.com/pay/api/android/reference/object#PaymentMethodTokenizationSpecification">PaymentMethodTokenizationSpecification</a>
      */
-    public static PaymentDataRequest createPaymentDataRequest(TransactionInfo transactionInfo) {
-        PaymentMethodTokenizationParameters.Builder paramsBuilder =
-                PaymentMethodTokenizationParameters.newBuilder()
-                        .setPaymentMethodTokenizationType(
-                                WalletConstants.PAYMENT_METHOD_TOKENIZATION_TYPE_PAYMENT_GATEWAY)
-                        .addParameter("gateway", Constants.GATEWAY_TOKENIZATION_NAME);
-        for (Pair<String, String> param : Constants.GATEWAY_TOKENIZATION_PARAMETERS) {
-            paramsBuilder.addParameter(param.first, param.second);
-        }
+    private static JSONObject getGatewayTokenizationSpecification() throws JSONException {
+        JSONObject tokenizationSpecification = new JSONObject();
 
-        return createPaymentDataRequest(transactionInfo, paramsBuilder.build());
+        tokenizationSpecification.put("type", "PAYMENT_GATEWAY");
+        JSONObject parameters = new JSONObject(Constants.GATEWAY_TOKENIZATION_PARAMETERS);
+        tokenizationSpecification.put("parameters", parameters);
+
+        return tokenizationSpecification;
     }
 
     /**
-     * Builds {@link PaymentDataRequest} for use with DIRECT integration to be consumed by
-     * {@link PaymentsClient#loadPaymentData}.
-     * <p>
-     * Please refer to the documentation for more information about DIRECT integration. The type of
+     * Direct Integration: Decrypt a response directly on your servers. This configuration has
+     * additional data security requirements from Google and additional PCI DSS compliance complexity.
+     *
+     * <p>Please refer to the documentation for more information about DIRECT integration. The type of
      * integration you use depends on your payment processor.
      *
-     * @param transactionInfo contains the price for this transaction.
+     * @return Payment data tokenization for the CARD payment method.
+     * @throws JSONException
+     * @see <a
+     *     href="https://developers.google.com/pay/api/android/reference/object#PaymentMethodTokenizationSpecification">PaymentMethodTokenizationSpecification</a>
      */
-    public static PaymentDataRequest createPaymentDataRequestDirect(TransactionInfo transactionInfo) {
-        PaymentMethodTokenizationParameters params =
-                PaymentMethodTokenizationParameters.newBuilder()
-                        .setPaymentMethodTokenizationType(
-                                WalletConstants.PAYMENT_METHOD_TOKENIZATION_TYPE_DIRECT)
+    private static JSONObject getDirectTokenizationSpecification() throws JSONException {
 
+        JSONObject tokenizationSpecification = new JSONObject();
+        tokenizationSpecification.put("type", "DIRECT");
+        tokenizationSpecification.put(
+                "parameters",
+                new JSONObject()
+                        .put("protocolVersion", "ECv1")
                         // Omitting the publicKey will result in a request for unencrypted data.
-                        // Please refer to the documentation for more information on unencrypted
-                        // requests.
-                        .addParameter("publicKey", Constants.DIRECT_TOKENIZATION_PUBLIC_KEY)
-                        .build();
-
-        return createPaymentDataRequest(transactionInfo, params);
-    }
-
-    private static PaymentDataRequest createPaymentDataRequest(TransactionInfo transactionInfo, PaymentMethodTokenizationParameters params) {
-        PaymentDataRequest request =
-                PaymentDataRequest.newBuilder()
-                        .setPhoneNumberRequired(false)
-                        .setEmailRequired(true)
-                        .setShippingAddressRequired(true)
-
-                        // Omitting ShippingAddressRequirements all together means all countries are
-                        // supported.
-                        .setShippingAddressRequirements(
-                                ShippingAddressRequirements.newBuilder()
-                                        .addAllowedCountryCodes(Constants.SHIPPING_SUPPORTED_COUNTRIES)
-                                        .build())
-
-                        .setTransactionInfo(transactionInfo)
-                        .addAllowedPaymentMethods(Constants.SUPPORTED_METHODS)
-                        .setCardRequirements(
-                                CardRequirements.newBuilder()
-                                        .addAllowedCardNetworks(Constants.SUPPORTED_NETWORKS)
-                                        .setAllowPrepaidCards(true)
-                                        .setBillingAddressRequired(true)
-
-                                        // Omitting this parameter will result in the API returning
-                                        // only a "minimal" billing address (post code only).
-                                        .setBillingAddressFormat(WalletConstants.BILLING_ADDRESS_FORMAT_FULL)
-                                        .build())
-                        .setPaymentMethodTokenizationParameters(params)
-
-                        // If the UI is not required, a returning user will not be asked to select
-                        // a card. Instead, the card they previously used will be returned
-                        // automatically (if still available).
-                        // Prior whitelisting is required to use this feature.
-                        .setUiRequired(true)
-                        .build();
-
-        return request;
+                        // Please refer to the documentation for more information on unencrypted requests.
+                        .put("publicKey", Constants.DIRECT_TOKENIZATION_PUBLIC_KEY));
+        return tokenizationSpecification;
     }
 
     /**
-     * Determines if the user is eligible to Pay with Google by calling
-     * {@link PaymentsClient#isReadyToPay}. The nature of this check depends on the methods set in
-     * {@link Constants#SUPPORTED_METHODS}.
-     * <p>
-     * If {@link WalletConstants#PAYMENT_METHOD_CARD} is specified among supported methods, this
-     * function will return true even if the user has no cards stored. Please refer to the
-     * documentation for more information on how the check is performed.
+     * Card networks supported by your app and your gateway.
      *
-     * @param client used to send the request.
+     * <p>TODO: Confirm card networks supported by your app and gateway & update in Constants.java.
+     *
+     * @return Allowed card networks
+     * @see <a
+     *     href="https://developers.google.com/pay/api/android/reference/object#CardParameters">CardParameters</a>
      */
-    public static Task<Boolean> isReadyToPay(PaymentsClient client) {
-        IsReadyToPayRequest.Builder request = IsReadyToPayRequest.newBuilder();
-        for (Integer allowedMethod : Constants.SUPPORTED_METHODS) {
-            request.addAllowedPaymentMethod(allowedMethod);
+    private static JSONArray getAllowedCardNetworks() {
+        JSONArray allowedCardNetworks = new JSONArray();
+        for (String network : Constants.SUPPORTED_NETWORKS) {
+            allowedCardNetworks.put(network);
         }
-        return client.isReadyToPay(request.build());
+        return allowedCardNetworks;
     }
 
     /**
-     * Builds {@link TransactionInfo} for use with {@link PaymentsUtil#createPaymentDataRequest}.
-     * <p>
-     * The price is not displayed to the user and must be in the following format: "12.34".
-     * {@link PaymentsUtil#microsToString} can be used to format the string.
+     * Card authentication methods supported by your app and your gateway.
      *
-     * @param price total of the transaction.
+     * <p>TODO: Confirm your processor supports Android device tokens on your supported card networks
+     * and make updates in Constants.java.
+     *
+     * @return Allowed card authentication methods.
+     * @see <a
+     *     href="https://developers.google.com/pay/api/android/reference/object#CardParameters">CardParameters</a>
      */
-    public static TransactionInfo createTransaction(String price) {
-        return TransactionInfo.newBuilder()
-                .setTotalPriceStatus(WalletConstants.TOTAL_PRICE_STATUS_FINAL)
-                .setTotalPrice(price)
-                .setCurrencyCode(Constants.CURRENCY_CODE)
-                .build();
+    private static JSONArray getAllowedCardAuthMethods() {
+        JSONArray allowedCardAuthMethods = new JSONArray();
+        for (String method : Constants.SUPPORTED_METHODS) {
+            allowedCardAuthMethods.put(method);
+        }
+        return allowedCardAuthMethods;
     }
 
     /**
-     * Converts micros to a string format accepted by {@link PaymentsUtil#createTransaction}.
+     * Describe your app's support for the CARD payment method.
+     *
+     * <p>The provided properties are applicable to both an IsReadyToPayRequest and a
+     * PaymentDataRequest.
+     *
+     * @return A CARD PaymentMethod object describing accepted cards.
+     * @throws JSONException
+     * @see <a
+     *     href="https://developers.google.com/pay/api/android/reference/object#PaymentMethod">PaymentMethod</a>
+     */
+    private static JSONObject getBaseCardPaymentMethod() throws JSONException {
+        JSONObject cardPaymentMethod = new JSONObject();
+        cardPaymentMethod.put("type", "CARD");
+
+        JSONObject parameters = new JSONObject();
+        parameters.put("allowedAuthMethods", getAllowedCardAuthMethods());
+        parameters.put("allowedCardNetworks", getAllowedCardNetworks());
+        // Optionally, you can add billing address/phone number associated with a CARD payment method.
+        parameters.put("billingAddressRequired", true);
+
+        JSONObject billingAddrParams = new JSONObject();
+        billingAddrParams.put("format", "FULL");
+
+        parameters.put("billingAddressParameters", billingAddrParams);
+
+        cardPaymentMethod.put("parameters", parameters);
+
+        return cardPaymentMethod;
+    }
+
+    /**
+     * Describe the expected returned payment data for the CARD payment method
+     *
+     * @return A CARD PaymentMethod describing accepted cards and optional fields.
+     * @throws JSONException
+     * @see <a
+     *     href="https://developers.google.com/pay/api/android/reference/object#PaymentMethod">PaymentMethod</a>
+     */
+    private static JSONObject getCardPaymentMethod() throws JSONException {
+        JSONObject cardPaymentMethod = getBaseCardPaymentMethod();
+        cardPaymentMethod.put("tokenizationSpecification", getGatewayTokenizationSpecification());
+
+        return cardPaymentMethod;
+    }
+
+    /**
+     * An object describing accepted forms of payment by your app, used to determine a viewer's
+     * readiness to pay.
+     *
+     * @return API version and payment methods supported by the app.
+     * @see <a
+     *     href="https://developers.google.com/pay/api/android/reference/object#IsReadyToPayRequest">IsReadyToPayRequest</a>
+     */
+    public static Optional<JSONObject> getIsReadyToPayRequest() {
+        try {
+            JSONObject isReadyToPayRequest = getBaseRequest();
+            isReadyToPayRequest.put(
+                    "allowedPaymentMethods", new JSONArray().put(getBaseCardPaymentMethod()));
+
+            // An optional shipping address requirement is a top-level property of the PaymentDataRequest
+            // JSON object. Omitting all together means all countries are supported.
+            isReadyToPayRequest.put("shippingAddressRequired", true);
+
+            JSONObject shippingAddrParams = new JSONObject();
+            shippingAddrParams.put("phoneNumberRequired", false);
+
+            JSONArray allowedCountryCodes = new JSONArray();
+            for (String country : Constants.SHIPPING_SUPPORTED_COUNTRIES) {
+                allowedCountryCodes.put(country);
+            }
+
+            shippingAddrParams.put("allowedCountryCodes", allowedCountryCodes);
+            isReadyToPayRequest.put("shippingAddressParameters", shippingAddrParams);
+
+            return Optional.of(isReadyToPayRequest);
+        } catch (JSONException e) {
+            return Optional.empty();
+        }
+    }
+
+    /**
+     * Provide Google Pay API with a payment amount, currency, and amount status.
+     *
+     * @return information about the requested payment.
+     * @throws JSONException
+     * @see <a
+     *     href="https://developers.google.com/pay/api/android/reference/object#TransactionInfo">TransactionInfo</a>
+     */
+    private static JSONObject getTransactionInfo(String price) throws JSONException {
+        JSONObject transactionInfo = new JSONObject();
+        transactionInfo.put("totalPrice", price);
+        transactionInfo.put("totalPriceStatus", "FINAL");
+        transactionInfo.put("currencyCode", Constants.CURRENCY_CODE);
+
+        return transactionInfo;
+    }
+
+    /**
+     * Information about the merchant requesting payment information
+     *
+     * @return Information about the merchant.
+     * @throws JSONException
+     * @see <a
+     *     href="https://developers.google.com/pay/api/android/reference/object#MerchantInfo">MerchantInfo</a>
+     */
+    private static JSONObject getMerchantInfo() throws JSONException {
+        return new JSONObject().put("merchantName", "Example Merchant");
+    }
+
+    /**
+     * An object describing information requested in a Google Pay payment sheet
+     *
+     * @return Payment data expected by your app.
+     * @see <a
+     *     href="https://developers.google.com/pay/api/android/reference/object#PaymentDataRequest">PaymentDataRequest</a>
+     */
+    public static Optional<JSONObject> getPaymentDataRequest(String price) {
+        try {
+            JSONObject paymentDataRequest = PaymentsUtil.getBaseRequest();
+            paymentDataRequest.put(
+                    "allowedPaymentMethods", new JSONArray().put(PaymentsUtil.getCardPaymentMethod()));
+            paymentDataRequest.put("transactionInfo", PaymentsUtil.getTransactionInfo(price));
+            paymentDataRequest.put("merchantInfo", PaymentsUtil.getMerchantInfo());
+            return Optional.of(paymentDataRequest);
+        } catch (JSONException e) {
+            return Optional.empty();
+        }
+    }
+
+    /**
+     * Converts micros to a string format accepted by {@link PaymentsUtil#getPaymentDataRequest}.
      *
      * @param micros value of the price.
      */
