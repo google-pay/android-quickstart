c4fac1ef7e04ff95ec265425c657c341b8b0aa43
Jose L Ugia <joseluisugia@gmail.com>
2019-01-11 00:39:06 +0100

    Add t-shirt shop basics

--- kotlin/app/src/main/java/com/google/android/gms/samples/wallet/util/Json.kt
+++ kotlin/app/src/main/java/com/google/android/gms/samples/wallet/util/Json.kt
@@ -11,10 +11,21 @@ class Json {
             try {
                 val inputStream:InputStream = context.getAssets().open(fileName)
                 val inputString = inputStream.bufferedReader().use{it.readText()}
-                return JSONArray(inputString);
+                return JSONArray(inputString)
 
             } catch (e:Exception){
-                return JSONArray();
+                return JSONArray()
+            }
+        }
+
+        fun readFromResources(context: Context, resource: Int) : JSONArray {
+            try {
+                val inputStream:InputStream = context.resources.openRawResource(resource)
+                val inputString = inputStream.bufferedReader().use{it.readText()}
+                return JSONArray(inputString)
+
+            } catch (e:Exception){
+                return JSONArray()
             }
         }
     }
