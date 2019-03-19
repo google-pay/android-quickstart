464c7ad00b592ae1b0af227be65e59726912300a
Jose L Ugia <joseluisugia@gmail.com>
2019-03-19 20:28:56 +0100

    Replace code with steps to do a follow along DIY integration

--- kotlin/app/src/main/java/com/google/android/gms/samples/wallet/CheckoutActivity.kt
+++ kotlin/app/src/main/java/com/google/android/gms/samples/wallet/CheckoutActivity.kt
@@ -17,21 +17,15 @@
 package com.google.android.gms.samples.wallet
 
 import android.app.Activity
-import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import android.text.Html
 import android.util.Log
-import android.view.View
-import android.widget.Toast
-import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.samples.wallet.util.Json
 import com.google.android.gms.wallet.*
 import kotlinx.android.synthetic.main.activity_checkout.*
 import org.json.JSONArray
-import org.json.JSONException
 import org.json.JSONObject
-import kotlin.math.roundToLong
 
 
 /**
@@ -45,7 +39,6 @@ class CheckoutActivity : Activity() {
      * @see [PaymentsClient](https://developers.google.com/android/reference/com/google/android/gms/wallet/PaymentsClient)
      */
     private lateinit var paymentsClient: PaymentsClient
-    private val shippingCost = (90 * 1000000).toLong()
 
     private lateinit var garmentList: JSONArray
     private lateinit var selectedGarment: JSONObject
@@ -65,29 +58,17 @@ class CheckoutActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_checkout)
+        fetchAndShowRandomGarment()
 
-        // Set up the mock information for our item in the UI.
-        selectedGarment = fetchRandomGarment()
-        displayGarment(selectedGarment)
-
-        // Initialize a Google Pay API client for an environment suitable for testing.
+        // 1. Initialize a Google Pay API client for an environment suitable for testing.
         // It's recommended to create the PaymentsClient object inside of the onCreate method.
-        paymentsClient = PaymentsUtil.createPaymentsClient(this)
+
+
+        // 2. Implement the method that determines whether or not to show the Google Pay button
         possiblyShowGooglePayButton()
 
+        // Add click listener to Google Pay button
         googlePayButton.setOnClickListener { requestPayment() }
-    }
-
-    private fun displayGarment(garment:JSONObject) {
-        detailTitle.setText(garment.getString("title"))
-        detailPrice.setText("\$${garment.getString("price")}")
-
-        val escapedHtmlText:String = Html.fromHtml(garment.getString("description")).toString()
-        detailDescription.setText(Html.fromHtml(escapedHtmlText))
-
-        val imageUri = "@drawable/${garment.getString("image")}"
-        val imageResource = resources.getIdentifier(imageUri, null, packageName)
-        detailImage.setImageResource(imageResource)
     }
 
     /**
@@ -98,20 +79,13 @@ class CheckoutActivity : Activity() {
     ) */
     private fun possiblyShowGooglePayButton() {
 
-        val isReadyToPayJson = PaymentsUtil.isReadyToPayRequest() ?: return
-        val request = IsReadyToPayRequest.fromJson(isReadyToPayJson.toString()) ?: return
+        // 3. Populate fields under the readyToPay request and turn it
+        // into a json string before calling isReadyToPay
+        // val isReadyToPayJson = ...
 
-        // The call to isReadyToPay is asynchronous and returns a Task. We need to provide an
-        // OnCompleteListener to be triggered when the result of the call is known.
-        val task = paymentsClient.isReadyToPay(request)
-        task.addOnCompleteListener { completedTask ->
-            try {
-                completedTask.getResult(ApiException::class.java)?.let(::setGooglePayAvailable)
-            } catch (exception: ApiException) {
-                // Process error
-                Log.w("isReadyToPay failed", exception)
-            }
-        }
+        // 4. Use the previously formed object to call isReeadyToPay and capture the result
+        // val task = paymentsClient.isReadyToPay(request)
+        // task.addOnCompleteListener...: setGooglePayAvailable(available)
     }
 
     /**
@@ -122,14 +96,14 @@ class CheckoutActivity : Activity() {
      * @param available isReadyToPay API response.
      */
     private fun setGooglePayAvailable(available: Boolean) {
-        if (available) {
-            googlePayButton.visibility = View.VISIBLE
-        } else {
-            Toast.makeText(
-                    this,
-                    "Unfortunately, Google Pay is not available on this device",
-                    Toast.LENGTH_LONG).show();
-        }
+        // 5. Show the Google Pay button if the user is able to pay using Google Pay
+        // Otherwise, update your UI and show notifications accordingly.
+    }
+
+    private fun requestPayment() {
+
+        // 6. Construct the payment data request object and initiate the payment transaction
+        // by calling loadPaymentData on the payments client.
     }
 
     /**
@@ -146,10 +120,9 @@ class CheckoutActivity : Activity() {
             // value passed in AutoResolveHelper
             LOAD_PAYMENT_DATA_REQUEST_CODE -> {
                 when (resultCode) {
-                    Activity.RESULT_OK ->
-                        data?.let { intent ->
-                            PaymentData.getFromIntent(intent)?.let(::handlePaymentSuccess)
-                        }
+                    Activity.RESULT_OK -> {
+                        // 7. Process the result of the transaction upon successful completion
+                    }
                     Activity.RESULT_CANCELED -> {
                         // Nothing to do here normally - the user simply cancelled without selecting a
                         // payment method.
@@ -176,44 +149,9 @@ class CheckoutActivity : Activity() {
      * Data](https://developers.google.com/pay/api/android/reference/object.PaymentData)
      */
     private fun handlePaymentSuccess(paymentData: PaymentData) {
-        val paymentInformation = paymentData.toJson() ?: return
-
-        try {
-            // Token will be null if PaymentDataRequest was not constructed using fromJson(String).
-            val paymentMethodData = JSONObject(paymentInformation).getJSONObject("paymentMethodData")
-
-            // If the gateway is set to "example", no payment information is returned - instead, the
-            // token will only consist of "examplePaymentMethodToken".
-            if (paymentMethodData
-                            .getJSONObject("tokenizationData")
-                            .getString("type") == "PAYMENT_GATEWAY" && paymentMethodData
-                            .getJSONObject("tokenizationData")
-                            .getString("token") == "examplePaymentMethodToken") {
-
-                AlertDialog.Builder(this)
-                        .setTitle("Warning")
-                        .setMessage("Gateway name set to \"example\" - please modify " +
-                                "Constants.java and replace it with your own gateway.")
-                        .setPositiveButton("OK", null)
-                        .create()
-                        .show()
-            }
-
-            val billingName = paymentMethodData.getJSONObject("info")
-                    .getJSONObject("billingAddress").getString("name")
-            Log.d("BillingName", billingName)
-
-            Toast.makeText(this, getString(R.string.payments_show_name, billingName), Toast.LENGTH_LONG).show()
-
-            // Logging token string.
-            Log.d("GooglePaymentToken", paymentMethodData
-                    .getJSONObject("tokenizationData")
-                    .getString("token"))
-
-        } catch (e: JSONException) {
-            Log.e("handlePaymentSuccess", "Error: " + e.toString())
-        }
-
+        // 8. Inspect the resulting payload of the transaction and perform any necessary
+        // action as a result of the transaction
+        // val paymentInformation = paymentData.toJson() ?: return
     }
 
     /**
@@ -229,33 +167,6 @@ class CheckoutActivity : Activity() {
         Log.w("loadPaymentData failed", String.format("Error code: %d", statusCode))
     }
 
-    // This method is called when the Pay with Google button is clicked.
-    private fun requestPayment() {
-
-        // Disables the button to prevent multiple clicks.
-        googlePayButton.isClickable = false
-
-        // The price provided to the API should include taxes and shipping.
-        // This price is not displayed to the user.
-        val garmentPriceMicros = (selectedGarment.getDouble("price") * 1000000).roundToLong()
-        val price = (garmentPriceMicros + shippingCost).microsToString()
-
-        val paymentDataRequestJson = PaymentsUtil.getPaymentDataRequest(price)
-        if (paymentDataRequestJson == null) {
-            Log.e("RequestPayment", "Can't fetch payment data request")
-            return
-        }
-        val request = PaymentDataRequest.fromJson(paymentDataRequestJson.toString())
-
-        // Since loadPaymentData may show the UI asking the user to select a payment method, we use
-        // AutoResolveHelper to wait for the user interacting with it. Once completed,
-        // onActivityResult will be called with the result.
-        if (request != null) {
-            AutoResolveHelper.resolveTask(
-                    paymentsClient.loadPaymentData(request), this, LOAD_PAYMENT_DATA_REQUEST_CODE)
-        }
-    }
-
     private fun fetchRandomGarment() : JSONObject {
         if (!::garmentList.isInitialized) {
             garmentList = Json.readFromResources(this, R.raw.tshirts)
@@ -263,5 +174,22 @@ class CheckoutActivity : Activity() {
 
         val randomIndex:Int = Math.round(Math.random() * (garmentList.length() - 1)).toInt()
         return garmentList.getJSONObject(randomIndex)
+    }
+
+    private fun fetchAndShowRandomGarment() {
+        selectedGarment = fetchRandomGarment()
+        displayGarment(selectedGarment)
+    }
+
+    private fun displayGarment(garment:JSONObject) {
+        detailTitle.setText(garment.getString("title"))
+        detailPrice.setText("\$${garment.getString("price")}")
+
+        val escapedHtmlText:String = Html.fromHtml(garment.getString("description")).toString()
+        detailDescription.setText(Html.fromHtml(escapedHtmlText))
+
+        val imageUri = "@drawable/${garment.getString("image")}"
+        val imageResource = resources.getIdentifier(imageUri, null, packageName)
+        detailImage.setImageResource(imageResource)
     }
 }
