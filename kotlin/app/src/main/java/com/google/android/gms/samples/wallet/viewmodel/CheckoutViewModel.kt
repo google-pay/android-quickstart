df5406d095b23b9083815a3cd30e3175aa9e6f7d
Jose Ugia <ugia@google.com>
2022-03-30 11:45:03 +0200

    Update hero image

--- kotlin/app/src/main/java/com/google/android/gms/samples/wallet/viewmodel/CheckoutViewModel.kt
+++ kotlin/app/src/main/java/com/google/android/gms/samples/wallet/viewmodel/CheckoutViewModel.kt
@@ -159,7 +159,7 @@ class CheckoutViewModel(application: Application) : AndroidViewModel(application
                 },
                 "heroImage": {
                   "sourceUri": {
-                    "uri": "https://storage.googleapis.com/wallet-lab-tools-codelab-artifacts-public/google-io-hero-demo-only-2.png"
+                    "uri": "https://storage.googleapis.com/wallet-lab-tools-codelab-artifacts-public/google-io-hero-demo-only.png"
                   }
                 },
                 "notifications": {
