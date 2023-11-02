6b02d3d8ad147382e6c1043067447237d5ac0954
Jose Ugia <ugia@google.com>
2023-11-02 10:16:09 +0100

    Bump library versions

--- kotlin/app/build.gradle.kts
+++ kotlin/app/build.gradle.kts
@@ -9,11 +9,12 @@ android {
 
     defaultConfig {
         applicationId = "com.google.android.gms.samples.wallet"
-        minSdk = 19
+        minSdk = 21
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
         }
@@ -38,18 +39,39 @@ android {
         jvmTarget = "1.8"
     }
 
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.2"
+    }
+
     buildFeatures {
+        compose = true
         viewBinding = true
+    }
+
+    packaging {
+        resources {
+            excludes.add("/META-INF/{AL2.0,LGPL2.1}")
+        }
     }
 }
 
 dependencies {
     val lifecycleVersion = "2.6.2"
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.gms:play-services-wallet:19.2.1")
+    implementation("com.google.android.gms:play-services-pay:16.3.0")
+    implementation("com.google.pay.button:compose-pay-button:0.1.0-beta03")
+    implementation("com.google.wallet.button:compose-wallet-button:0.1.0-beta01")
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.compose.foundation:foundation:1.4.0")
+    implementation("androidx.compose.material3:material3:1.1.2")
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
-    implementation("androidx.activity:activity-ktx:1.7.0")
+    implementation("androidx.activity:activity-ktx:1.7.2")
+
+    androidTestImplementation("androidx.test.uiautomator:uiautomator:2.2.0")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.4")
 }
