1416de650a5248c044c27290814c43c7cf76b51b
Jose Ugia <ugia@google.com>
2024-01-22 13:55:40 +0100

    Bump versions

--- kotlin/app/build.gradle.kts
+++ kotlin/app/build.gradle.kts
@@ -56,10 +56,10 @@ android {
 }
 
 dependencies {
-    val lifecycleVersion = "2.6.2"
+    val lifecycleVersion = "2.7.0"
 
-    implementation("com.google.android.gms:play-services-wallet:19.2.1")
-    implementation("com.google.pay.button:compose-pay-button:0.1.1")
+    implementation("com.google.android.gms:play-services-wallet:19.3.0-beta01")
+    implementation("com.google.pay.button:compose-pay-button:0.1.3")
 
     val composeBom = platform("androidx.compose:compose-bom:2023.10.01")
     implementation(composeBom)
@@ -67,13 +67,13 @@ dependencies {
     implementation("androidx.compose.material3:material3")
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleVersion")
-    implementation("androidx.activity:activity-ktx:1.8.1")
+    implementation("androidx.activity:activity-ktx:1.8.2")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.1")
 
     androidTestImplementation("androidx.test.uiautomator:uiautomator:2.2.0")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.4")
